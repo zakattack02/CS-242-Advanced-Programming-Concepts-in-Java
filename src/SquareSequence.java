256ec1d21a04eeda13a010845758ea95860971dc
Dwight Tuinstra <dwight.tuinstra@gmail.com>
2025-10-14 21:11:00 -0400

    SquareSequence: fix it so first element is 0.

--- src/SquareSequence.java
+++ src/SquareSequence.java
@@ -2,6 +2,11 @@ public class SquareSequence implements Sequence
 {
     private int n;
 
+    public SquareSequence() {
+        // start at -1 so first element delivered is 0.
+        n = -1;
+    }
+
     public int next()
     {
         n++;
